bc16d1685c80979c8508d33542ea156f3212ffc6
Aymane <aymanebk@gmail.com>
2023-09-19 22:53:44 +0100

    basic functionality

--- game.js
+++ game.js
@@ -2,10 +2,14 @@ const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let locked = false;
+
+let win = 10;
 
 function flipCard(){
-    this.classList.add('flip');
-    
+    if(locked) return;
+    if(this === firstCard) return;
+    this.classList.add('flip'); 
     if(!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
@@ -13,7 +17,6 @@ function flipCard(){
     }
 
     secondCard = this;
-    hasFlippedCard = false;
 
     checkMatch();
 }
@@ -31,14 +34,38 @@ function checkMatch(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    reset();
+    win--;
 }
 
 function unflipCards(){
+    locked = true;
     setTimeout(()=>{
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');   
+
+        reset();
     }, 1500);
+
 }
+
+function reset(){
+    [hasFlippedCard, locked] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function checkWin(){
+    if (win === 0){
+        alert('CONGRATULATIONS');
+    }
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 20);
+      card.style.order = ramdomPos;
+    });
+  })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
